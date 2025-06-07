4cffc817c8b89dc29e970ed36055a8c3b82d75ae
Dharshansk16 <dcodecraft@gmail.com>
2025-06-07 17:16:40 +0530

    chore:customise django-settings

---
+++ config/settings/local.py
@@ -0,0 +1,10 @@
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-po&5^)c0*hkg+s+1cvf_kw!xkg-q$j2o&nh84gd#+)4uyow9*x'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
