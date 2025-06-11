47b76f06a02ac2874e4ec9658afecaaf555043e1
Dharshansk16 <dcodecraft@gmail.com>
2025-06-11 17:12:12 +0530

    feat:setup api, postgres, mailpit containers using docker

---
+++ docker/local/django/entrypoint.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -o errexit
+
+set -o pipefail
+
+set -o nounset
+
+python << END
+
+import sys
+import time
+import psycopg2
+suggest_unrecoverable_after =30
+start=time.time()
+
+while True:
+    try:
+        psycopg2.connect(
+        dbname="${POSTGRES_DB}",
+        user="${POSTGRES_USER}",
+        password="${POSTGRES_PASSWORD}",
+        host="${POSTGRES_HOST}",
+        port="${POSTGRES_PORT}",
+        )
+        break
+    except psycopg2.operationalError as error:
+        sys.stderr.write("Waiting for PostgreSQL to become available...\n")
+        if time.time() -start > suggest_unrecoverable_after:
+            sys.stderr.write(
+                f"This is taking longer than expected. The following exception is "
+                f"indicative of an unrecoverable error: '{error}'\n"
+            )
+        time.sleep(3)
+END
+
+echo >&2 'PostgreSQL is available'
+
+exec "$@" 
